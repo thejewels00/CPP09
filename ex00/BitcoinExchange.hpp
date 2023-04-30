ca6aafdf27ed28c15f5010677bedc489a9ecf277
Jawaher <jchennak@e2r7p5.1337.ma>
2023-04-30 17:17:36 +0100

    cpp-09

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,39 @@
 /*   By: jchennak <jchennak@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 16:57:26 by jchennak          #+#    #+#             */
-/*   Updated: 2023/04/19 16:57:27 by jchennak         ###   ########.fr       */
+/*   Updated: 2023/04/30 17:04:34 by jchennak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <string>
+#include <iostream>
+#include <map>
+#include <ctime>
+#include <fstream>
+
+class BitcoinExchange {
+  
+  private :
+    std::string csvfile;
+    std::map<std::tm, int> bitcoin;
+    
+  public :
+    BitcoinExchange();
+    BitcoinExchange(std::string csvfile);
+    ~BitcoinExchange();
+    BitcoinExchange(const BitcoinExchange &src);
+    BitcoinExchange & operator=(const BitcoinExchange &src);
+    
+    class Error_in_file_opening: public std::exception {
+        const char* what() const throw();
+    };
+    
+    class Line_error: public std::exception {
+        const char* what() const throw();
+    };
+};
+
+#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,13 +6,26 @@
 /*   By: jchennak <jchennak@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 16:57:21 by jchennak          #+#    #+#             */
-/*   Updated: 2023/04/19 18:22:49 by jchennak         ###   ########.fr       */
+/*   Updated: 2023/04/29 18:18:49 by jchennak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <fstream>
-#include <filesystem>
+#include "BitcoinExchange.hpp"
+
+
+// void extension_file(std::string file)
+// {
+//     size_t      dot_pos;
+//     std::string extension;
+    
+//     if((dot_pos = file.find_last_of(".")) != std::string::npos)
+//         extension = file.substr(dot_pos + 1); // take as a second argument npos
+//     if(extension != "csv")
+//     {
+//         std::cerr << "Usage: " << file << " <csv_file> : CSV FILE !!" << std::endl;
+//         exit(1);
+//     }
+// }
 
 
 int main(int argc, char **argv)
@@ -21,6 +34,8 @@ int main(int argc, char **argv)
     {
         
         std::string csvfile = argv[1];
+       // extension_file(csvfile);
+        
         // check the extention
         // open the file
         // read the file
